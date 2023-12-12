9af6af8c006fac479ba6b7d79922ca7d652e02a1
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2023-12-12 12:00:29 -0500

    actualizacion links america latina

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,13 @@
 <div class="container">
 
     <div class="mb-3">
-        <a href="https://www.facebook.com/arrepienteteypreparaelcamino" target="_blank" > <img src="{{ asset('images/ico-facebook.svg')}}"/> </a>
-        <a href="https://www.instagram.com/arrepienteteypreparaelcamino" target="_blank" > <img src="{{ asset('images/ico-ig.svg')}}" /> </a>
-        <a href="https://api.whatsapp.com/send?phone=51922774790" target="_blank" > <img src="{{ asset('images/ico-whatsapp.svg')}}" /> </a>
-        <a href="https://twitter.com/ArrepienteteRd" target="_blank" > <img src="{{ asset('images/ico-twitter.svg')}}" /> </a>
-        <a href="https://vimeo.com/user140572643" target="_blank" > <img src="{{ asset('images/ico-youtube.svg')}}" /> </a>
+        <a href="https://www.facebook.com/arrepentimientoPeru" target="_blank" > <img src="{{ asset('images/ico-facebook.svg')}}"/> </a>
+        <a href="https://api.whatsapp.com/send?phone=51906335546" target="_blank" > <img src="{{ asset('images/ico-whatsapp.svg')}}" /> </a>
+        <a href="https://twitter.com/msantolatino" target="_blank" > <img src="{{ asset('images/ico-twitter.svg')}}" /> </a>
+        <a href="https://www.youtube.com/@MinisterioSantoAmericaLatina" target="_blank" > <img src="{{ asset('images/ico-youtube.svg')}}" /> </a>
+        <a href="https://www.tiktok.com/@msantolatino" target="_blank" > <img src="{{ asset('images/ico-tiktok.svg')}}" /> </a>
     </div>
 
     <p>© 2021 - Ministerio de Arrepentimiento y Santidad - América Latina - Todos los derechos reservados<br>
-        <strong> https://www.laescaleraalcielo.org     |    E-mail: admin@laescaleraalacielo.org </strong></p>
-</div>  
+        <strong> https://www.laescaleraalcielo.org     |    E-mail: ministeriosantoamericalatina@gmail.com </strong></p>
+</div>
